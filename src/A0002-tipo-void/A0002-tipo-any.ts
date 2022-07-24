59be4968fa2aff76c4be6f0b90815501e79952d6
Leonardo Zabotto <leozabotto02@gmail.com>
2022-07-24 18:17:19 -0300

    any type

--- src/A0002-tipo-void/A0002-tipo-any.ts
+++ src/A0002-tipo-void/A0002-tipo-any.ts
@@ -2,6 +2,16 @@ function semRetorno(...args: string[]): void {
   console.log(args.join(' '));
 }
 
+const pessoa = {
+  nome: 'Leo',
+  sobrenome: 'Zabotto',
 
+  exibirNome(): void {
+    console.log(this.nome + ' ' + this.sobrenome);
+  },
+};
 
 semRetorno('Leo', 'Zabotto');
+pessoa.exibirNome();
+
+export { pessoa };
